9003bc66fc042bba0a2de0124251d0d89a30a540
dominguezsantal <alexdominguezwebdev@gmail.com>
2021-08-15 02:14:24 -0700

    add ui dependencies.It will import ui files/function for getting rate

--- src/main.js
+++ src/main.js
@@ -2,3 +2,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchange from '.src/exchanger.js';
+
+
+function getRates(response, conversionAmount, currencyCode, currencyText) {
+    if (response["conversion_rates"][currencyCode] === undefined) {
+        $(".showErrors").text(`The selected currency code is not available`);
+    } else if (response.result === "success") {
+        $(".showConversion").text(`${conversionAmount} USD = ${(response["conversion_rates"][currencyCode]*[conversionAmount]).toLocaleString()} ${currencyText}`);
+    } else {
+        $(".showErrors").text(`There was an error processing your request: ${response["error-type"]}`);
+    }
+}
